c53281603f593656c83ae4816551c94b13001ec9
Bianca Aspin <bianca@mj94.net>
2023-02-07 10:18:04 -0800

    Add event listeners to form input fields.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,38 @@
+import {useState} from 'react';
+
 function Form() {
+    const formObject = {
+        description: "",
+        location: "",
+        image: ""
+    }
+    
+    const [formInput, setFormInput] = useState(formObject)
+
+    function handleInput(e) {
+        const key = e.target.name
+        const value = e.target.value
+        const updatedFormInput = {
+            ...formInput,
+            [key]: value
+        }
+        setFormInput(updatedFormInput);
+
+    }
+
     return (
         <form>
             <label>
                 Description:
-                <input type="text" name="description" />
+                <input type="text" name="description" value={formInput.description} onChange={handleInput}/>
             </label>
             <label>
                 Location:
-                <input type="text" name="location" />
+                <input type="text" name="location" value={formInput.location} onChange={handleInput}/>
             </label>
             <label>
                 Image:
-                <input type="text" name="image" />
+                <input type="text" name="image" value={formInput.image} onChange={handleInput}/>
             </label>
             <input type="submit" value="Submit!" />
         </form>
